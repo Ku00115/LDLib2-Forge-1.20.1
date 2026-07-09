43d07a1d941791df642959d8d47fb108c4ab7e56
kuzhizaizheli <3635798774@qq.com>
2026-07-09 13:30:58 +0800

    Fix remaining production mixin targets

--- src/main/java/com/lowdragmc/lowdraglib2/core/mixins/shader/ShaderInstanceMixin.java
+++ src/main/java/com/lowdragmc/lowdraglib2/core/mixins/shader/ShaderInstanceMixin.java
@@ -13,26 +13,25 @@ import net.minecraft.server.packs.resources.ResourceProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.IOException;
 
 @Mixin(ShaderInstance.class)
 public abstract class ShaderInstanceMixin implements ILDShaderInstance {
-    @Inject(method = "<init>(Lnet/minecraft/server/packs/resources/ResourceProvider;Lnet/minecraft/resources/ResourceLocation;Lcom/mojang/blaze3d/vertex/VertexFormat;)V",
-            require = 1,
-            at = {@At(
+    @ModifyExpressionValue(method = "<init>(Lnet/minecraft/server/packs/resources/ResourceProvider;Lnet/minecraft/resources/ResourceLocation;Lcom/mojang/blaze3d/vertex/VertexFormat;)V",
+            at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/client/renderer/ShaderInstance;getOrCreate(Lnet/minecraft/server/packs/resources/ResourceProvider;Lcom/mojang/blaze3d/shaders/Program$Type;Ljava/lang/String;)Lcom/mojang/blaze3d/shaders/Program;",
                     ordinal = 1
-            )})
-    public void ldlib2$onCreateShader(ResourceProvider resourceProvider,
-                                      ResourceLocation shaderLocation,
-                                      VertexFormat vertexFormat,
-                                      CallbackInfo ci,
-                                      @Local JsonObject json) throws IOException {
+            ))
+    public Program ldlib2$onCreateShader(Program original,
+                                         ResourceProvider resourceProvider,
+                                         ResourceLocation shaderLocation,
+                                         VertexFormat vertexFormat,
+                                         @Local JsonObject json) throws IOException {
         this.onCreateShader(resourceProvider, shaderLocation, vertexFormat, json);
+        return original;
     }
 
     @Inject(method = "getOrCreate", at = {@At(value = "HEAD")}, cancellable = true)
